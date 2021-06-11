73a317b34352d4b9d83b9b022d9307d51de3acc0
argos44 <52904579+argos44@users.noreply.github.com>
2021-06-11 14:04:58 +0200

    Update VideoGPUBanchmark.java
    en work

--- VideoGPUBanchmark.java
+++ VideoGPUBanchmark.java
@@ -1,55 +1,36 @@
 /*program tests the speed of the various generations of graphics cards, because it ignores hardware acceleration */
 
+/*
+ * File: gfxtest.java
+ * --------------------
+
+
+ */
+
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
-import java.lang.Math.*;
 
-VideoGPUBanchmark extends GCanvas 
- {
+public class gfxtest extends GraphicsProgram {
+
+	public static final double PHI = 1.666013;
 	
-        /**
-    *
-    */
-    
-    private static final long serialVersionUID = 1L; //
+	public void run() {
+	   for (i = 1 : i=i+1 : i<360 ) {
+		
+		GCanvas FeltBored = new GCanvas(); 
+	 GOval oval = new GOval(150, 50 + 50 / PHI, 100, 100 / PHI);
+	  oval.setFilled(true);
+	  
+	  FeltBored.add(oval);
+	  
+	   Iterator iterationTerra = FeltBored.iterator();
+	   }
+	}
 
-    public int renderPolySideLengthProgram() /*the side of the polygon dra(w)n succesively is generated in a random (pseudorandom) way */
-        {
-         /* Would use Math.random, because the alternative is dependant on the CPU too*/
-
-         /*so, it would not cleanly...... measure the speed of the graphics card. RAMDAC, etc, is another story */
-         
-         double side.useless = Math.random();
-    
-        }
-
-    /*public void setColor(Color.MAGENTA);  */ 
-
-    /*4b8b8b 4b8b8b*/
-    /*4b8bc8b 4b8b8b*/
-
-    /*inverse sink*/
-
- /*randomPolySideLength.side, and this (which I never understood, it IS confusing, is not necessary (probably), 'cause they are global variables, or the equivalent')*/
-    
-    public void run() {
-
-        for (int i = 0; i < 5000; i++) 
-            {
-                private GPolygon createHexagon(final double side.useless)
-                 {
-                    final GPolygon hex = new GPolygon();
-                    hex.addVertex(-side, 0);
-                    for (int z = 0; z < 5; z++) 
-                     {
-                      hex.addPolarEdge(side, 60 - z * 60);
-                     }
-                    return hex;
-                 }
-            }    
-         
- 
-        
-	
-	https://www.goodreads.com/book/show/2255968.Hermann_Hesse_Das_Glasperlenspiel All I know Clue: Bárdos Alice, Hanna Senes
+/* Standard Java entry point */
+/* This method can be eliminated in most Java environments */
+	public static void main(String[] args) {
+		new gfxtest().start(args);
+	}
+}
